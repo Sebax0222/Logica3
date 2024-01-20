c3d52ec6c1b149e7c0f0dd56fd22ec7e2fd03521
Sebax0222 <sebax.0222lg@gmail.com>
2024-01-20 11:48:16 -0500

    Correccion y explicacion ejercicio 1

--- ejercicio 1/calculadora.py
+++ ejercicio 1/calculadora.py
@@ -1,19 +1,36 @@
-# def calculadora():
-num1 = input("Ingrese el primer número: ")
-num2 = input("Ingrese el segundo número: ")
-operacion = input("Ingrese la operación (+, -, *, /): ")
+#define la funcion calculadora
+def calculadora():
+    
+ # La linea 2, 3 y 4 Definen 3 varias, 2 tipo numero y otra tipo string para definir los numeros a operar y la operacion
+ 
+    num1 = float(input("Ingrese el primer número: ")) ##Se agrega los inputs en las variables num1 y 2
+    num2 = float(input("Ingrese el segundo número: ")) ##Se agrega los inputs en las variables num1 y 2
+    operacion = input("Ingrese la operación (+, -, *, /): ")
 
-if operacion == '+':
-        resultado = num1 + num2
-elif operacion == '-':
+# Desde la linea 6 a la 15 hay un bloque de condicionales if, elif y else donde se definen las operaciones a realizar 
+# segun los datos ingresados
+    if operacion == '+':
+        resultado = num1 + num2 ##Se corrige las variables num1 en los condicionales
+    elif operacion == '-':
         resultado = num1 - num2
-elif operacion == '*':
-        resultado = num1 * num2
-elif operacion == '/':
+    elif operacion == '*':
+        resultado = num1 * num2 ##Se corrige las variables num1 en los condicionales
+    elif operacion == '/':
         resultado = num1 / num2
-else:
-     resultado = "Operación no válida"
+    else:
+        resultado = "Operación no válida"
+        
+# El return de devuelve el resultado de la operacion y el print imprime el resultado
+    return print(resultado) ##se agrega el return un el print final y se elimina el texto por problemas de concatenacion
 
-print(f"Resultado: ", resultado)
+# Llamado de la funcion
+calculadora() ##Se corrige la palabra calculadora
 
-# calculadora()
+
+
+
+
+
+
+
+#
