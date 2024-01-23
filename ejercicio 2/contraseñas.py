f5e660d492593c6849d8912df00134dc2c060ef2
Sebax0222 <sebax.0222lg@gmail.com>
2024-01-22 19:30:10 -0500

    correcion y explicacion ejercicio 2

--- ejercicio 2/contraseñas.py
+++ ejercicio 2/contraseñas.py
@@ -1,9 +1,9 @@
-import rando
+import random #Se completa la palambra ramdom
 import string
 
-generar_contraseña(longitud=8):
+def generar_contraseña(longitud=8):#Se define la funcion agregando def 
     caracteres = string.ascii_letters + string.digits + string.punctuation
     contraseña = ''.join(random.choice(caracteres) for _ in range(longitud))
-    retunr contraseña
+    return contraseña #Se escribe correctamente la palabra return
 
-print(generar_contrasena())
+print(generar_contraseña()) #Se escribe correctamente la palabra contraseÑa
--- ejercicio 3/buscadorPalabras.py
+++ ejercicio 3/buscadorPalabras.py
@@ -1,7 +1,7 @@
-def contar_palabra(texto, palabra)
+def contar_palabra(texto, palabra):#Se agregan los dos puntos 
     return texto.lower().split().count(palabra.lower())
 
 texto = "Este es un ejemplo de texto. Este texto tiene palabras repetidas."
 palabra_buscada = "texto"
 
-print(f"La palabra '{palabra_buscada aparece contar_palabra(texto, palabra_buscada)} veces.")
+print(f"La palabra '{palabra_buscada}' aparece {contar_palabra(texto, palabra_buscada)} veces.") #Se agrega } en palabra_buscada y se agrega { en el inicio de contar_palabra
