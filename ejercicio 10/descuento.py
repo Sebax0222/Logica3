7092f9aef74ccb8c7fa381771af3f52cd5110a21
Sebax0222 <sebax.0222lg@gmail.com>
2024-01-27 11:35:05 -0500

    correcion y explicacion ejercicio 10

--- ejercicio 10/descuento.py
+++ ejercicio 10/descuento.py
@@ -1,9 +1,16 @@
-calcular_precio_con_decuento(precio_base, porcentaje_descuento)
+#Se define la funcion y sus parametros
+def calcular_precio_con_descuento(precio_base, porcentaje_descuento): ##Se agrega def y los puntos finales, se corrige la palabra descuento
+    #Se define la variable descuento con la formula para el descuento
     descuento = precio_base * (porcentaje_descuento / 100)
+    #Se define el precio final aplicando el descuento
     precio_final = precio_base - descuento
-    retunr precio_final
+    return precio_final ##Se corrige la palabra return
 
-precio_base = float("Ingrese el precio base del producto: ")
+#Se solicitan los datos al usuario
+precio_base = float(input("Ingrese el precio base del producto: ")) ##Se agrega el input
 porcentaje_descuento = float(input("Ingrese el porcentaje de descuento: "))
+
+#Se llama a la funcion para realizar el calculo y el porcentaje de descuento
 precio_final = calcular_precio_con_descuento(precio_base, porcentaje_descuento)
+#Se imprime el resultado del descuento segun el resultado de la funcion
 print(f"El precio final con {porcentaje_descuento}% de descuento es: {precio_final}")
--- ejercicio 9/dados.py
+++ ejercicio 9/dados.py
@@ -1,10 +1,16 @@
+#Se importa la libreria random
 import random ##Se corrige la palabra random
 
+#Se define la funciona con sus argumentos
 def simular_lanzamiento_dados(cantidad_dados, caras_por_dado): ##Se define la funcion agregando def
+    #se define la variable resultados como una lista donde en el rango estipulado en los inputs se generan numeros aleatorios
     resultados = [random.randint(1, caras_por_dado) for _ in range(cantidad_dados)]
+    #Se retorna la variable anterior
     return resultados ##Se corrige la palabra return
 
+#Se definen las variables con las que se va a trabajar
 cantidad_dados = int(input("Ingrese la cantidad de dados a lanzar: "))
 caras_por_dado = int(input("Ingrese la cantidad de caras por dado: "))
-lanzamientos = simular_lanzamiento_dados(cantidad_dados, caras_por_dado)
+#Se llama a la funciona y se guarda el resultado en la variable lanzamientos la cual es impresa posteriormente
+lanzamientos = simular_lanzamiento_dados(cantidad_dados, caras_por_dado)##Se agrega el argumento faltante
 print(f"Resultados del lanzamiento: {lanzamientos}")
