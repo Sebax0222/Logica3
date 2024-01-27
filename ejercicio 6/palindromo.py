f95ee82c82eb14ec81abda77b15210a185921d72
Sebax0222 <sebax.0222lg@gmail.com>
2024-01-27 10:42:57 -0500

    Correccion y explicacion ejercicio 7

--- ejercicio 6/palindromo.py
+++ ejercicio 6/palindromo.py
@@ -1,9 +1,15 @@
+
+#Se define la funcion es_palindromo en la cual segun un suceso de instrucciones se convierte un texo en una cadena 
+# en minuscula.
 def es_palindromo(texto): ##Se agregan 2 puntos al final
     texto = ''.join(caracter.lower() for caracter in texto if caracter.isalnum())
+    #Se compara el texto inicial con el mismo a la inversa [::-1] para verificar si la palabra es un palindromo
     return texto == texto[::-1]
 
+#Se solicita al usuario ungresar una palabra
 palabra = input("Ingrese una palabra o frase: ")
-if es_palindromo(palabra):
+#Con condicionales se llama a la funciona y se predeterminan mensajes segun sea true o false la respuesta de la funciona.
+if es_palindromo(palabra): ##Se agrega el argumento segun la palabra anexa por el usuario.
     print(f"{palabra} es un palíndromo.")
 else:
     print(f"{palabra} no es un palíndromo.")
--- ejercicio 7/factorial.py
+++ ejercicio 7/factorial.py
@@ -1,8 +1,13 @@
-def factorial(n)
-    if n == 0 or n = 1
-        retunr 1
+#Se define la funcion factorial con n como argumento
+def factorial(n):##Se agregan los dos puntos
+    #SE realiza un condicional para verificar que el argumento cumpla la condicion ser igual a 0 o 1 y retornar un 1 para esos casos
+    if n == 0 or n == 1: ##Se agrega = para completar la comparacion y se agregan los dos puntos finales
+        return 1 ##Se escribe correctamente el return
     else:
-        retunr n * factorial(n - 1)
+        #Se evidencia una funcion recursiva ya que se llama asi misma con el fin  de ir disminuyendo desde el numero dado hasta 1, para poder realizar el factorial
+        return n * factorial(n - 1) ##Se escribe correctamente el return
 
+#Se solicita al usuario ingresar un numero entero
 numero = int(input("Ingrese un número para calcular su factorial: "))
-print(f"El factorial de {numero} es {factorial()}")
+#Se imprime el numero con el resultado de la funciona
+print(f"El factorial de {numero} es {factorial(numero)}") ##Se agrega el argumento de la funcion llamada
